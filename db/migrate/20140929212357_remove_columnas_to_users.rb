3ca2d829fcbc8bd50ee9025b582b1f775d3d7e28
fabohack <fabianrjimenez@gmail.com>
2014-09-29 16:58:37 -0500

    Finish user signup

---
+++ db/migrate/20140929212357_remove_columnas_to_users.rb
@@ -0,0 +1,5 @@
+class RemoveColumnasToUsers < ActiveRecord::Migration
+  def change
+    remove_column :users, :password_digest, :string
+  end
+end
---
+++ db/migrate/20140929213016_add_columnas_to_users.rb
@@ -0,0 +1,6 @@
+class AddColumnasToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :password, :string
+    add_column :users, :password_confirmation, :string
+  end
+end
