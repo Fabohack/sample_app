a36eaf58ed6a268c820a521ded8209de86021ae3
fabohack <fabianrjimenez@gmail.com>
2014-09-30 17:22:17 -0500

    Finish user edit, update, index, and destroy actions

---
+++ db/migrate/20140930214108_add_columna_to_users.rb
@@ -0,0 +1,4 @@
+class AddColumnaToUsers < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140930192551) do
+ActiveRecord::Schema.define(version: 20140930221424) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20140930192551) do
     t.string   "password"
     t.string   "password_confirmation"
     t.string   "remember_digest"
+    t.string   "password_digest"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
