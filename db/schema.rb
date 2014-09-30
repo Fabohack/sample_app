d988bf2a2be765a31d4d4701b2eb28e98bcf5e2e
fabohack <fabianrjimenez@gmail.com>
2014-09-30 15:27:23 -0500

    Finish log in/log out

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140929213016) do
+ActiveRecord::Schema.define(version: 20140930192551) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140929213016) do
     t.datetime "updated_at",            null: false
     t.string   "password"
     t.string   "password_confirmation"
+    t.string   "remember_digest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
